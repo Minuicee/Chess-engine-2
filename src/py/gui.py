fabbf704dc356e580e6fb8db1bc837342b981c78
Minuicee <jasonlembcke@gmail.com>
2025-07-25 20:45:58 +0200

    Update gitignore to ignore pycache and start implement gui

--- src/py/gui.py
+++ src/py/gui.py
@@ -1,12 +1,44 @@
-from PyQt5.QtWidgets import QHBoxLayout, QLineEdit, QMainWindow, QApplication, QLabel, QGridLayout, QWidget, QVBoxLayout, QPushButton, QColorDialog, QSlider, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, QHBoxLayout
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-import sys
+
+SQUARE_IMAGES = ['../../images/square_w.png', '../../images/square_b.png']
 
 class window(QMainWindow):
-    def __init__(self, board_size=1000, extra_space=200):
+
+    def __init__(self, board_size, extra_space):
         super().__init__()
+
+        # init variables
+        self.board_size = board_size
+        self.extra_space = extra_space
+        self.squares = []
+
+        # init gui 
         self.init_gui()
 
     def init_gui(self):
-        self.setGeometry(board_size + extra_space, board_size)
+        # set frame variables
+        self.setFixedSize(self.board_size + self.extra_space, self.board_size)
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+
+        # grid for the squares
+        self.square_grid = QWidget()
+        self.grid_layout = QGridLayout()
+        self.square_grid.setLayout(self.grid_layout)
+
+        # init squares
+        for i in range(8):
+            for j in range(8):
+
+                # do this to every square
+                square = QPushButton()
+                position = ((i+1)+j*8) 
+                square.setStyleSheet(f"background-image: url('{SQUARE_IMAGES[0] if position % 2 == 0 else SQUARE_IMAGES[1]}');")
+                self.squares.append(square)
+                self.grid_layout.addWidget(square, i, j)
+
+
+        # add widgets to window
+        self.layout.addWidget(self.square_grid)
--- src/py/main.py
+++ src/py/main.py
@@ -1,8 +1,16 @@
-
+from PyQt5.QtWidgets import QApplication
+import sys
+import gui
 
 class main():
-    def __init__(self):
-        pass
+
+    def __init__(self, board_size=800, extra_space=200):
+        # init the frame
+        app = QApplication(sys.argv)
+        window = gui.window(board_size, extra_space)
+        window.show()
+        sys.exit(app.exec())
 
 if __name__ == "__main__":
-    game = main()
+    game = main() 
+
